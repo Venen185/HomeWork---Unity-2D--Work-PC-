08201d9c99aab041829ce4607f3b9e1d832e8b0b
Venen185 <63510242+Venen185@users.noreply.github.com>
2020-04-28 08:07:25 +0300

    home 28042020

--- 2D Plaform (work)/Assets/Script/PlayerMove.cs
+++ 2D Plaform (work)/Assets/Script/PlayerMove.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    Animator animator;
+    Rigidbody2D rigidBody2DMovement;
+    SpriteRenderer spiriteRendered;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rigidBody2DMovement = GetComponent<Rigidbody2D>();
+        spiriteRendered = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rigidBody2DMovement.velocity = new Vector2(10, rigidBody2DMovement.velocity.y);
+            animator.Play("Player_run");
+            spiriteRendered.flipX = false;
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rigidBody2DMovement.velocity = new Vector2(-10, rigidBody2DMovement.velocity.y);
+            animator.Play("Player_run");
+            spiriteRendered.flipX = true;
+        }
+        else
+        {
+            animator.Play("Player_idle");
+            rigidBody2DMovement.velocity = new Vector2(0, rigidBody2DMovement.velocity.y);
+        }
+
+        if (Input.GetKey("space") || Input.GetKey("up"))
+        {
+            rigidBody2DMovement.velocity = new Vector2(rigidBody2DMovement.velocity.x, 3);
+            animator.Play("Player_jump");
+        }
+
     }
 }
